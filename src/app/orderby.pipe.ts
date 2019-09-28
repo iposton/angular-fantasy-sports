2084b07fdaed5980d936ad988bd46a4a3238fc73
iposton <ianposton@sbcglobal.net>
2019-09-28 14:39:11 -0700

    fix pipe sort. fix col width. fix rank background.

--- src/app/orderby.pipe.ts
+++ src/app/orderby.pipe.ts
@@ -6,15 +6,24 @@ export class OrderBy implements PipeTransform {
 
 transform(array: any[], field: string): any[] {
     array.sort((a: any, b: any) => {
-      if (a[field] <= b[field] || 
-        a.offensePlayers[0].playerObj[field] <= b.offensePlayers[0].playerObj[field]) {
-        return -1;
-      } else if (a[field] >= b[field] || 
-        a.offensePlayers[0].playerObj[field] >= b.offensePlayers[0].playerObj[field]) {
-        return 1;
-      } else {
-        return 0;
+      if (field === 'mlb') {
+        if (a['gameId'] <= b['gameId']) {
+          return -1;
+        } else if (a['gameId'] >= b['gameId']) {
+          return 1;
+        } else {
+          return 0;
+        }
+      } else if (field === 'nfl') {
+        if (a.offensePlayers[0].playerObj['gameId'] <= b.offensePlayers[0].playerObj['gameId']) {
+            return -1;
+          } else if (a.offensePlayers[0].playerObj['gameId'] >= b.offensePlayers[0].playerObj['gameId']) {
+            return 1;
+          } else {
+            return 0;
+          }
       }
+      
     });
     return array;
   }
