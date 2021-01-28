396db5d6cca664f32dc30341806e2667b25aba63
iposton <ianposton@sbcglobal.net>
2021-01-28 14:49:58 -0800

    nhl nba player images. add new goalie. refactor.

--- src/app/modules/starting-five/starting-five.component.ts
+++ src/app/modules/starting-five/starting-five.component.ts
@@ -158,28 +158,28 @@ export class StartingFiveComponent implements OnInit {
     }
   }
 
-  public colorLuminance(hex, lum) {
+  // public colorLuminance(hex, lum) {
 
-    // validate hex string
-    hex = String(hex).replace(/[^0-9a-f]/gi, '');
-    if (hex.length < 6) {
-      hex = hex[0]+hex[0]+hex[1]+hex[1]+hex[2]+hex[2];
-    }
-    lum = lum || 0;
+  //   // validate hex string
+  //   hex = String(hex).replace(/[^0-9a-f]/gi, '');
+  //   if (hex.length < 6) {
+  //     hex = hex[0]+hex[0]+hex[1]+hex[1]+hex[2]+hex[2];
+  //   }
+  //   lum = lum || 0;
   
-    // convert to decimal and change luminosity
-    var rgb = "#", c, i;
-    for (i = 0; i < 3; i++) {
-      c = parseInt(hex.substr(i*2,2), 16);
-      c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
-      rgb += ("00"+c).substr(c.length);
-    }
+  //   // convert to decimal and change luminosity
+  //   var rgb = "#", c, i;
+  //   for (i = 0; i < 3; i++) {
+  //     c = parseInt(hex.substr(i*2,2), 16);
+  //     c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
+  //     rgb += ("00"+c).substr(c.length);
+  //   }
   
-    return rgb;
-  }
+  //   return rgb;
+  // }
 
   public getBackground(color) {
-    let lighter = this.colorLuminance(color, 0.6);
+    let lighter = this.util.colorLuminance(color, 0.6);
     if (color === "#c4ced4") {
       color = "#000000";
       lighter = "#555555";
