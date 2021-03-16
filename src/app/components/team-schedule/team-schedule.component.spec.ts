b38272bba315d5ac13d1b986fc4b8b3e48948e6c
iposton <ianposton@sbcglobal.net>
2021-03-16 16:06:55 -0700

    update angular 11. fix active links. fix team schedule. add new goalie.

--- src/app/components/team-schedule/team-schedule.component.spec.ts
+++ src/app/components/team-schedule/team-schedule.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TeamScheduleComponent } from './team-schedule.component';
 
@@ -6,7 +6,7 @@ describe('TeamScheduleComponent', () => {
   let component: TeamScheduleComponent;
   let fixture: ComponentFixture<TeamScheduleComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TeamScheduleComponent ]
     })
--- src/app/modules/stat-leaders/stat-leaders.component.spec.ts
+++ src/app/modules/stat-leaders/stat-leaders.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { StatLeadersComponent } from './stat-leaders.component';
 
@@ -6,7 +6,7 @@ describe('StartingFiveComponent', () => {
   let component: StatLeadersComponent;
   let fixture: ComponentFixture<StatLeadersComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ StatLeadersComponent ]
     })
